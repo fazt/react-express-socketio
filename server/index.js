16fe3c4cbd15fe72f691ca20af4e82b0b2531320
fazt <fazt@faztweb.com>
2022-06-14 22:34:55 -0500

    added static files

--- server/index.js
+++ server/index.js
@@ -21,9 +21,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 // Middlewares
 app.use(cors());
 app.use(morgan("dev"));
-app.use(express.static(join(__dirname, "public")));
-
 app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(join(__dirname, "../client/build")));
 
 io.on("connection", (socket) => {
   console.log(socket.id);
